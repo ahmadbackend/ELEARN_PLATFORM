1e6d04e59fe0eace7bd6dec63b7b6d07b74bef47
ahmadbackend <ahmadbasherjsphp@gmail.com>
2024-08-30 20:17:36 +0300

    blocking bad user from chatting fixing sender bug

--- ELEARN_BACKEND/HOME_AREA/routing.py
+++ ELEARN_BACKEND/HOME_AREA/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
 from .consumers import *
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>[\w\s-]+)/$", ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>.+)/(?P<course>.+)/$', ChatConsumer.as_asgi()),
 ]
